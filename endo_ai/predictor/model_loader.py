19a736c0fde30b12dfe4f23d4b0700645ba9d228
maddonix <tlux14@googlemail.com>
2025-02-21 00:22:56 +0100

    update ai model / meta stuff

--- endo_ai/predictor/model_loader.py
+++ endo_ai/predictor/model_loader.py
@@ -7,23 +7,44 @@ from sklearn.metrics import precision_score, recall_score, f1_score
 
 METRICS_ON_STEP = False
 
+
 def calculate_metrics(pred, target, threshold=0.5):
     pred = np.array(pred > threshold, dtype=float)
-    return {'micro/precision': precision_score(y_true=target, y_pred=pred, average='micro', zero_division = 0),
-            'micro/recall': recall_score(y_true=target, y_pred=pred, average='micro', zero_division = 0),
-            'micro/f1': f1_score(y_true=target, y_pred=pred, average='micro', zero_division = 0),
-            'macro/precision': precision_score(y_true=target, y_pred=pred, average='macro', zero_division = 0),
-            'macro/recall': recall_score(y_true=target, y_pred=pred, average='macro', zero_division = 0),
-            'macro/f1': f1_score(y_true=target, y_pred=pred, average='macro', zero_division = 0),
-            'samples/precision': precision_score(y_true=target, y_pred=pred, average=None, zero_division = 0),
-            'samples/recall': recall_score(y_true=target, y_pred=pred, average=None, zero_division = 0),
-            'samples/f1': f1_score(y_true=target, y_pred=pred, average=None, zero_division = 0),
-            }
+    return {
+        "micro/precision": precision_score(
+            y_true=target, y_pred=pred, average="micro", zero_division=0
+        ),
+        "micro/recall": recall_score(
+            y_true=target, y_pred=pred, average="micro", zero_division=0
+        ),
+        "micro/f1": f1_score(
+            y_true=target, y_pred=pred, average="micro", zero_division=0
+        ),
+        "macro/precision": precision_score(
+            y_true=target, y_pred=pred, average="macro", zero_division=0
+        ),
+        "macro/recall": recall_score(
+            y_true=target, y_pred=pred, average="macro", zero_division=0
+        ),
+        "macro/f1": f1_score(
+            y_true=target, y_pred=pred, average="macro", zero_division=0
+        ),
+        "samples/precision": precision_score(
+            y_true=target, y_pred=pred, average=None, zero_division=0
+        ),
+        "samples/recall": recall_score(
+            y_true=target, y_pred=pred, average=None, zero_division=0
+        ),
+        "samples/f1": f1_score(
+            y_true=target, y_pred=pred, average=None, zero_division=0
+        ),
+    }
+
 
 class MultiLabelClassificationNet(LightningModule):
     def __init__(
         self,
-        labels=["ASD"],
+        labels=None,
         lr=6e-3,
         weight_decay=0.001,
         pos_weight=2,
@@ -128,4 +149,3 @@ class MultiLabelClassificationNet(LightningModule):
             "lr_scheduler": lr_scheduler,
             "monitor": "val/loss",
         }
-
