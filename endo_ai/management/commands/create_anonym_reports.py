eb2ccdd444a5a90916947bb335fc2e54cd5a8a25
maddonix <tlux14@googlemail.com>
2025-03-05 09:46:20 +0100

    implement rawVideoFile to Video

--- endo_ai/management/commands/create_anonym_reports.py
+++ endo_ai/management/commands/create_anonym_reports.py
@@ -24,6 +24,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         raw_pdfs = RawPdfFile.objects.all()  # pylint: disable=no-member
         for raw_pdf in raw_pdfs:
-            ic(raw_pdf)
             report_file = raw_pdf.get_or_create_report_file()
             ic(report_file)
+            ic(f"Text: {report_file.text}")
---
+++ endo_ai/management/commands/create_anonym_videos.py
@@ -0,0 +1,39 @@
+# from io import StringIO
+# from pathlib import Path
+from icecream import ic
+
+from django.core.management import BaseCommand  # , call_command
+from endoreg_db.models import (
+    # VideoSegmentationLabel,
+    # LabelSet,
+    # AiModel,
+    # ModelMeta,
+    RawVideoFile,
+)
+
+# from agl_frame_extractor import VideoFrameExtractor
+# from endo_ai.predictor.model_loader import MultiLabelClassificationNet
+
+
+# Example usage:
+
+# python manage.py censor_outside --raw_video_uuid 138c846e649a40eb84d6633c99f7e704
+
+
+class Command(BaseCommand):
+    """ """
+
+    help = """
+
+    """
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        raw_video_files = RawVideoFile.objects.all()  # pylint: disable=no-member
+
+        for raw_video in raw_video_files:
+            raw_video.censor_outside_frames()
+            video = raw_video.get_or_create_video()
+            ic(video)
